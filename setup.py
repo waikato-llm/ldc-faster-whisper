1845abe3d623f704e48dc07c6bd6215a5a6cd402
Peter Reutemann <fracpete@gmail.com>
2025-01-07 09:47:33 +1300

    using llm_dataset_converter now in dependencies

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     },
     packages=find_namespace_packages(where='src'),
     install_requires=[
-        "llm-dataset-converter>=0.2.3",
+        "llm_dataset_converter>=0.2.3",
         "faster-whisper",
     ],
     version="0.0.1",
